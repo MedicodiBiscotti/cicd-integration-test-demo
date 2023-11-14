b0058da7bbcac595a4ff64a12621d556b7f6569b
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2023-11-14 12:08:40 +0100

    init

---
+++ src/main/java/com/example/cicdintegrationtestdemo/CicdIntegrationTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdintegrationtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdIntegrationTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdIntegrationTestDemoApplication.class, args);
+    }
+
+}
